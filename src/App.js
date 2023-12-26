4c995a536ce7c26a539cee719e3175243a16a05d
= <=>
2023-12-26 13:53:15 +0530

    Added the Routes for Landing Screen

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import { Navbar } from "./Components/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import C0_LandingScreen from "./Screens/Screen1-LandingScreen/C0_LandingScreen";
+
 function App() {
   return (
     <div className="h-screen bg-white dark:bg-black ">
-     <Navbar/>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<C0_LandingScreen />}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
