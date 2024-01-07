02143e9fc55e8d87fb247f5e5de90b5fd999ff5d
Kahitoz <kshitiz.sinha.025@gmail.com>
2024-01-07 22:12:44 +0530

    Added S0_Dashboard

---
+++ src/Screens/Screen2-Students/S0_Dashboard.js
@@ -0,0 +1,10 @@
+const S0_Dashboard = () =>{
+    return(
+        <>
+        <div>
+            <p className="dark:text-white text-black">Welcome Student</p>
+        </div>
+        </>
+    );
+}
+export default S0_Dashboard;
