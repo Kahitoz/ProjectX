3ec89c827346e635ab20f2d8e0e91ebc621a25ff
Kahitoz <kshitiz.sinha.025@gmail.com>
2024-01-08 22:35:44 +0530

    Implemented the students api to check

--- src/Screens/Screen2-Students/S0_Dashboard.js
+++ src/Screens/Screen2-Students/S0_Dashboard.js
@@ -1,10 +1,57 @@
-const S0_Dashboard = () =>{
-    return(
-        <>
-        <div>
-            <p className="dark:text-white text-black">Welcome Student</p>
-        </div>
-        </>
-    );
-}
+import Cookies from "js-cookie";
+import { useNavigate } from "react-router-dom";
+import cred from "../../Components/Credential.json";
+import { useEffect, useState } from "react";
+
+const S0_Dashboard = () => {
+  const token = Cookies.get("ACCESS_TOKEN");
+  const [email, setEmail] = useState("");
+  const [role, setRole] = useState("");
+  const Navigate = useNavigate();
+  useEffect(() => {
+    if (token === undefined) {
+      Navigate("/");
+    }
+    const Get_userInfo = async () => {
+      const userData = await get_info();
+      setEmail(userData.email);
+      setRole(userData.role);
+    };
+    Get_userInfo();
+  }, []);
+
+  const HandleLogout = () => {
+    Cookies.remove("ACCESS_TOKEN");
+    Navigate("/");
+  };
+  return (
+    <>
+      <div>
+        <p className="dark:text-white text-black">Welcome {email}</p>
+        <p className="dark:text-white text-black">
+          You are signed in as {role}
+        </p>
+        <button
+          className="text-white bg-red-500 p-2"
+          onClick={() => HandleLogout()}
+        >
+          Logout
+        </button>
+      </div>
+    </>
+  );
+};
 export default S0_Dashboard;
+
+async function get_info() {
+  const info_api = cred.api.concat("/user/details");
+  const token = Cookies.get("ACCESS_TOKEN");
+  const api_response = await fetch(info_api, {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  const api_data = await api_response.json();
+  return api_data;
+}
